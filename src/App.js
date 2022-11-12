96dc6e33f47c854f0a5010ec6c386a1cd39944a1
SayedMajid <sydmajid04@gmail.com>
2022-11-12 11:44:43 +0530

    Move functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,208 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  FormControlLabel,
+  Checkbox,
+  Typography,
+  Button,
+} from "@mui/material";
+import "./App.css";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import { useEffect, useState } from "react";
+import { data } from "./data";
 
 function App() {
+  const [state, setState] = useState("move");
+  const [master, setMaster] = useState(data);
+  const [unique, setUnique] = useState();
+  const [elements, setElements] = useState([]);
+  console.log(state);
+
+  const selectMaster = (id) => {
+    setUnique(id);
+    const temp = master?.map((el) =>
+      el.id === id ? { ...el, selected: !el.selected } : el
+    );
+    setMaster(temp);
+  };
+
+  const selectElements = (id) => {
+    setUnique(id);
+    const temp = elements?.map((el) =>
+      el.id === id ? { ...el, selected: !el.selected } : el
+    );
+    setElements(temp);
+  };
+
+  const moveOneLeft = (unique) => {
+    if (unique) {
+      const temp = master?.find((el) => el.id === unique);
+      setElements([...elements, { ...temp, selected: false }]);
+      const filteredData = master?.filter((el) => el.id !== unique);
+      setMaster(filteredData);
+    }
+  };
+
+  const moveOneRight = (unique) => {
+    if (unique) {
+      const temp = elements?.find((el) => el.id === unique);
+      setMaster([...master, { ...temp, selected: false }]);
+      const filteredData = elements?.filter((el) => el.id !== unique);
+      setElements(filteredData);
+    }
+  };
+
+  const shiftAllLeft = () => {
+    if (state === "move") {
+      if (master.length > 0) {
+        selectElements([...elements, ...master]);
+        setMaster([]);
+        return;
+      }
+
+      setElements([...data]);
+      setMaster([]);
+    }
+  };
+
+  const shiftAllRight = () => {
+    if (state === "move") {
+      if (elements.length > 0) {
+        setMaster([...master, ...elements]);
+        setElements([]);
+        return;
+      }
+
+      setMaster([...data]);
+      setElements([]);
+    }
+  };
+
+  useEffect(() => {}, [master, elements]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Box border="1px solid black" padding="20px">
+        <Box
+          display="flex"
+          width="70%"
+          margin=" 20px auto"
+          justifyContent="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={state === "copy"}
+                size="medium"
+                value="copy"
+                onChange={(e) => setState("copy")}
+              />
+            }
+            label="Copy"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={state === "move"}
+                size="medium"
+                value="move"
+                onChange={(e) => setState("move")}
+              />
+            }
+            label="Move"
+          />
+        </Box>
+        <Box
+          m="auto"
+          justifyContent={"center"}
+          display="flex"
+          width="70%"
+          height="700px"
+          gap="20px"
+        >
+          <Box width="40%" border="1px solid" overflow="scroll" p="10px">
+            <Typography fontSize={"24px"} textAlign="center" padding="20px">
+              Master
+            </Typography>
+            <Box>
+              {master?.map((el, i) => (
+                <Box
+                  key={i}
+                  border="1px solid black"
+                  borderRadius="12px"
+                  p="18px"
+                  textAlign="center"
+                  fontSize="18px"
+                  letterSpacing="1.1px"
+                  fontWeight="500"
+                  mb="12px"
+                  cursor="pointer"
+                  className={el.selected ? "selected" : ""}
+                  onClick={() => selectMaster(el.id)}
+                >
+                  {el.name}
+                </Box>
+              ))}
+            </Box>
+          </Box>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent={"center"}
+            gap="20px"
+          >
+            <Button
+              size="large"
+              variant="outlined"
+              onClick={() => moveOneLeft(unique)}
+            >
+              <ArrowForwardIosIcon />
+            </Button>
+            <Button
+              size="large"
+              variant="outlined"
+              onClick={() => moveOneRight(unique)}
+            >
+              <ArrowBackIosNewIcon />
+            </Button>
+            <Button size="large" variant="outlined" onClick={shiftAllLeft}>
+              <KeyboardDoubleArrowRightIcon />
+            </Button>
+            <Button size="large" variant="outlined" onClick={shiftAllRight}>
+              <KeyboardDoubleArrowLeftIcon />
+            </Button>
+          </Box>
+          <Box width="40%" border="1px solid" overflow={"scroll"}>
+            <Typography fontSize={"24px"} textAlign="center" p="20px">
+              New Elements
+            </Typography>
+            <Box>
+              {elements.length > 0 &&
+                elements?.map((el, i) => (
+                  <Box
+                    key={i}
+                    border="1px solid black"
+                    p="18px"
+                    textAlign="center"
+                    mb="12px"
+                    borderRadius="12px"
+                    cursor="pointer"
+                    fontSize="18px"
+                    letterSpacing="1.1px"
+                    fontWeight="500"
+                    className={el.selected ? "selected" : ""}
+                    onClick={() => selectElements(el.id)}
+                  >
+                    {el.name}
+                  </Box>
+                ))}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    id: 1,
+    name: "JavaScript",
+    selected: false,
+  },
+  {
+    id: 2,
+    name: "HTML5",
+    selected: false,
+  },
+  {
+    id: 3,
+    name: "CSS3",
+    selected: false,
+  },
+  {
+    id: 4,
+    name: "ReactJs",
+    selected: false,
+  },
+  {
+    id: 5,
+    name: "Redux",
+    selected: false,
+  },
+  {
+    id: 6,
+    name: "Chakra Ui",
+    selected: false,
+  },
+  {
+    id: 7,
+    name: "Material Ui",
+    selected: false,
+  },
+  {
+    id: 8,
+    name: "NodeJs",
+    selected: false,
+  },
+  {
+    id: 9,
+    name: "ExpressJs",
+    selected: false,
+  },
+  {
+    id: 10,
+    name: "MongoDB",
+    selected: false,
+  },
+];
